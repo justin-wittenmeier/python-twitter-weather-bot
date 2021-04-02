11d526703966f4394f7ed04ce83f3661148bd3a3
justin-wittenmeier <71291781+justin-wittenmeier@users.noreply.github.com>
2021-04-02 15:04:35 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,34 +2,28 @@ import requests
 import tweepy
 import string
 
-auth = tweepy.OAuthHandler('twitter token','twitter token') # twitter login info
-auth.set_access_token('twitter token', 'twitter token') # twitter login info
-api = tweepy.API(auth)
+class Bot:
+    def __init__(self):
+        self.twitter_oauth = ['twitter token','twitter token'] # twitter login info
+        self.twitter_token = ['twitter token', 'twitter token'] # twitter login info
+        self.url = ' open weather map url ' # openweathermap.org url
 
-r = requests.get('open weather map url') # openweathermap.org api
+    def weatherUpdate(self):
+        auth = tweepy.OAuthHandler(self.twitter_oauth[0], self.twitter_oauth[1]) # twitter login info
+        auth.set_access_token(self.twitter_token[0], self.twitter_token[1]) # twitter login info
+        api = tweepy.API(auth)
+        r = requests.get(self.url)
+        json_data = r.json()
+        desc = json_data['weather'][0]['description']
+        temp = json_data['main']['temp']
+        feels_like = json_data['main']['feels_like']
+        wind_speed = round(json_data['wind']['speed'] * 2.237, 1)
+        fTemp = lambda x: round((x - 273.15) * 9/5 + 32)
+        user = api.me()
+        post_number = user.statuses_count + 1
+        manchester_weather = f'Weather in Manchester TN\n\nSky: {string.capwords(desc)}\nTemp: {fTemp(temp)}°F\nFeels Like: {fTemp(feels_like)}°F\nWind Speed: {wind_speed}mph\n\nI am a bot this was posted automatically\n#{post_number}'
+        api.update_status(manchester_weather)
 
-# data from open weather map
-json_data = r.json()
-
-# descrption of weather
-desc = json_data['weather'][0]['description']
-
-# real temp
-temp = json_data['main']['temp']
-
-# feels like temp
-feels_like = json_data['main']['feels_like']
-
-# translate wind speed to mph
-wind_speed = round(json_data['wind']['speed'] * 2.237, 1)
-
-# func that find the F temp from K
-fTemp = lambda x: round((x - 273.15) * 9/5 + 32)
-
-# get number of post from profile to prevent duplicate posts
-user = api.me()
-post_number = user.statuses_count + 1
-
-manchester_weather = f'Weather in Manchester TN\n\nSky: {string.capwords(desc)}\nTemp: {fTemp(temp)}°F\nFeels Like: {fTemp(feels_like)}°F\nWind Speed: {wind_speed}mph\n\nI am a bot this was posted automatically\n#{post_number}'
-
-api.update_status(manchester_weather)
+if __name__ == '__main__':
+    bot = Bot()
+    bot.weatherUpdate()
